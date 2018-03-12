08beaf04c997badda534e71d985b663341ad3298
Daniel Brazel <dbrazel4@gmail.com>
2018-03-12 18:48:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  return_array = []
+  array.each do |name|
+    return_array << "Hello, my name is #{name}."
+  end
+end
